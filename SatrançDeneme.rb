f0ca4a30e85931ccc046ad27f5ab16b0b41df970
SytronXar <aydemir_emre65@hotmail.com>
2020-12-18 22:25:14 +0300

    Update SatrançDeneme.rb

--- SatrançDeneme.rb
+++ SatrançDeneme.rb
@@ -301,7 +301,7 @@ def Terfi()
     d=$LMove.mYatay
     dogruSecim=false
     while dogruSecim==false
-        print "Tas secin:"
+        print "Tas secin örneğin: Vezir için 'v'":
         input=gets.chomp
         if TasKontrolu(input)
             $Tahta[c][d]= $sira%2==0 ? input.upcase : input.downcase
